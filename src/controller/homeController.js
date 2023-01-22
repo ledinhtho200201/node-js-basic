b336a1c229ddd19b4cb804a4389a8a87927894d2
JRabbit <ledinhtho686@gmail.com>
2023-01-22 09:28:22 +0700

    crud with update and delete

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -25,7 +25,27 @@ let createNewUser = async (req, res) => {
     return res.redirect('/')
 }
 
+let deleteUser = async (req, res) => {
+    let userId = req.body.userId;
+    await pool.execute('DELETE FROM users WHERE id = ?', [userId]);
+    return res.redirect('/')
+}
+
+let getEditPage = async (req, res) => {
+    let id = req.params.id;
+    let [user] = await pool.execute('SELECT * FROM users WHERE id = ?', [id]);
+
+    return res.render('update.ejs', { dataUser: user[0] }); // x <- y
+}
+
+let postUpdateUser = async (req, res) => {
+    let { firstName, lastName, email, address, id } = req.body;
+
+    await pool.execute('UPDATE users SET firstName = ?, lastName= ?, email = ?, address = ? WHERE id = ?', [firstName, lastName, email, address, id]);
+
+    return res.redirect('/');
+}
 
 module.exports = {
-    getHomepage, getDetailPage, createNewUser
+    getHomepage, getDetailPage, createNewUser, deleteUser, getEditPage, postUpdateUser
 }
